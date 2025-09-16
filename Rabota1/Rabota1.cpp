fa3f3c336564aef50871109f0e42e40ea31d3d75
lvero <lvero@LITTLE-GENIUS>
2025-09-16 20:51:07 +0300

    Добавьте файлы проекта.

---
+++ Rabota1/Rabota1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath> 
+#include <CTYPE.H>
+#include <conio.h> 
+#include <cstdlib>
+#include <ctime>
+#include <io.h>
+#include <fcntl.h>
+#include <cstring> 
+#include <string>
+#include <Windows.h>
+using namespace std;
+struct Person
+{
+    int startcap; //то что мы изначально имеем
+    int years;
+
+};
+struct deposit                 //вклад в сбере
+{
+    
+    void 
+};
+struct Mashasav
+{
+
+};
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+   
+}
